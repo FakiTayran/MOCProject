2f46aa7f2d10eaac40dc942e08433696ebe5b78c
FakiPC <aridurufakitayran@gmail.com>
2022-11-26 22:38:07 +0300

    Add project files.

---
+++ MOCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MOCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
