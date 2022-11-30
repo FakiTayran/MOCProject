237a4ee64c08f6f0c3f390e49a2b4ac235cf09bf
FakiPC <aridurufakitayran@gmail.com>
2022-11-30 22:02:12 +0300

    Department select added while new user register

---
+++ MOCProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MOCProject.Data;
+using MOCProject.Models;
+
+[assembly: HostingStartup(typeof(MOCProject.Areas.Identity.IdentityHostingStartup))]
+namespace MOCProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
